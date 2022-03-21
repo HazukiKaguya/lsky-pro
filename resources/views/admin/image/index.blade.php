b29e4d75f90f99cfdf9684e1dce1c0d2d9d7a6aa
Hazuki Kaguya <35645329+HazukiKaguya@users.noreply.github.com>
2022-03-22 01:51:29 +0900

    针对tif,psd，使用缩略图，其他均使用源文件
    针对tif,psd，使用缩略图，其他均使用源文件

--- resources/views/admin/image/index.blade.php
+++ resources/views/admin/image/index.blade.php
@@ -23,19 +23,28 @@
                             <span class="bg-white rounded-md text-sm px-1 py-0">Gif</span>
                         @endif
                         @if($image->extension === 'mp4')
-                            <span class="bg-white rounded-md text-sm px-1 py-0">mp4</span>
+                            <span class="bg-white rounded-md text-sm px-1 py-0">Video</span>
                         @endif
                         @if($image->extension === 'webm')
-                            <span class="bg-white rounded-md text-sm px-1 py-0">webm</span>
+                            <span class="bg-white rounded-md text-sm px-1 py-0">Video</span>
                         @endif
                     </div>
+                    //针对tif,psd，使用缩略图，其他均使用源文件
                     @if($image->extension === 'mp4')
                     <video class="w-full h-36 object-cover transition-all group-hover:brightness-50" src="{{ $image->url }} " controls/>
                     @endif
                     @if($image->extension === 'webm')
                     <video class="w-full h-36 object-cover transition-all group-hover:brightness-50" src="{{ $image->url }} " controls/>
                     @endif
-                    <img class="w-full h-36 object-cover transition-all group-hover:brightness-50" src="{{ $image->thumb_url }}">
+                    <img class="w-full h-36 object-cover transition-all group-hover:brightness-50" 
+                    @if($image->extension === 'tif')
+                    src="{{ $image->thumb_url }}"
+                    @elseif($image->extension === 'psd')
+                    src="{{ $image->thumb_url }}"
+                    @else
+                    src="{{ $image->url }}"
+                    @endif
+                    >
 
                     <div class="absolute top-2 right-2 space-x-1 hidden group-hover:flex">
                         <i data-id="{{ $image->id }}" class="delete fas fa-trash text-red-500 w-4 h-4"></i>
@@ -367,7 +376,6 @@
                 let image = $(this).data('json');
                 let htm = $('#image-tpl').html()
                     .replace(/__id__/g, image.id)
-                    .replace(/__url__/g, image.url)
                     .replace(/__user_name__/g, image.user ? image.user.name+'('+image.user.email+')' : '游客')
                     .replace(/__user_email__/g, image.user ? image.user.email : '-')
                     .replace(/__album_name__/g, image.album ? image.album.name : '-')
@@ -386,27 +394,48 @@
                     .replace(/__is_unhealthy__/g, image.is_unhealthy ? '<span class="text-red-500"><i class="fas fa-exclamation-triangle"></i> 是</span>' : '否')
                     .replace(/__uploaded_ip__/g, image.uploaded_ip)
                     .replace(/__created_at__/g, image.created_at);
+                // 检查是否为视频或者无法预览的psd、tif
                 let checkstr=image.url;
                 let check1="webm";
                 let check2="mp4";
+                let check3="psd";
+                let check4="tif";
                 if (checkstr.match(check1)){
                     let html=htm
+                    .replace(/__url__/g, image.url)
                     .replace(/<img/g, "<video")
                     .replace(/class="w-full object-center object-cover">/g, "class=\"w-full object-center object-cover\" controls/>");
 
                 $('#modal-content').html(html);
 
                 modal.open('content-modal')
-                }else if(checkstr.match(check2)){
+                }
+                else if(checkstr.match(check2)){
                     let html=htm
+                    .replace(/__url__/g, image.url)
                     .replace(/<img/g, "<video")
                     .replace(/class="w-full object-center object-cover">/g, "class=\"w-full object-center object-cover\" controls/>");
 
                 $('#modal-content').html(html);
 
                 modal.open('content-modal')
-                }else{
-                    let html = htm;
+                }
+                else if(checkstr.match(check3)){
+                    let html=htm.replace(/__url__/g, "/thumbnails/"+image.md5+".png");
+
+                $('#modal-content').html(html);
+
+                modal.open('content-modal')
+                }
+                else if(checkstr.match(check4)){
+                    let html=htm.replace(/__url__/g, "/thumbnails/"+image.md5+".png");
+
+                $('#modal-content').html(html);
+
+                modal.open('content-modal')
+                }
+                else{
+                    let html = htm.replace(/__url__/g, image.url);
 
                 $('#modal-content').html(html);
 
